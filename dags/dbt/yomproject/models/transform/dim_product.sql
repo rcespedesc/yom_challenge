16d1c32e245cb456cdd654504dc22fe83bd0b6ea
Roberto <bob@Robertos-MacBook-Pro.local>
2024-06-09 22:32:17 -0500

    feat: initial code

---
+++ dags/dbt/yomproject/models/transform/dim_product.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT
+    {{ dbt_utils.generate_surrogate_key(['StockCode', 'Description', 'UnitPrice']) }} as product_id,
+		StockCode AS stock_code,
+    Description AS description,
+    UnitPrice AS price
+FROM {{ ref('online_retail') }}
+WHERE StockCode IS NOT NULL
+AND UnitPrice > 0
